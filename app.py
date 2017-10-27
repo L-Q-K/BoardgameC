519ea2ee19fa2ac8983ab8e026900818ed2d3df6
L-Q-K <kykise@gmail.com>
2017-10-27 21:21:38 +0700

    New app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ mlab.connect()
 app = Flask(__name__)
 
 class Room_detail(Document):
-    player_name = ListField()
+    player_name = ListField(StringField())
 
 class Player_detail(Document):
     #char_image = StringField ()
@@ -23,12 +23,36 @@ class Role (Document):
     role_phe = StringField()
     role_ability = StringField()
 
+pn = []
+for i in range(15):
+    pn.append("Player " + str(i+1))
+player_names = Room_detail(player_name = pn)
+
+# player_names.save()
+
+
+room_details = Room_detail.objects()
+player_details = Player_detail.objects()
+roles = Role.objects()
+i = randint(1,17)
+r = roles [i]
+print(r["role_phe"])
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/1234')
 def room():
+    for j in range(20):
+        for player in room_details[j]["player_name"]:
+            i = randint(1,17)
+            r = roles[i]
+            p = Player_detail(name = player,
+                            role = r["role_name"],
+                            phe = r["role_phe"],
+                            ability = r["role_ability"]).save()
+
     return render_template('test.html',Room_detail = Room_detail.objects())#, Player_detail = Player_detail.objects())
 
 if __name__ == '__main__':
