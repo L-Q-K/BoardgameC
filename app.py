d805d24767f8ef80725397082df1848bf975b2c7
DESKTOP-CQ7S6R5\400DoiCan <tholominh@gmail.com>
2017-11-03 20:29:24 +0700

    push commit

--- app.py
+++ app.py
@@ -2,56 +2,89 @@ from flask import *
 import mlab
 from mongoengine import *
 from random import *
-
+import uuid
 
 mlab.connect()
 
 app = Flask(__name__)
 
 class Room_detail(Document):
+    acess_code = StringField()
     player_name = ListField(StringField())
-
-class Player_detail(Document):
-    #char_image = StringField ()
-    name = StringField ()
-    role = StringField ()
-    phe = StringField()
-    ability = StringField ()
 
 class Role (Document):
     role_name = StringField()
     role_phe = StringField()
     role_ability = StringField()
 
-pn = []
-for i in range(15):
-    pn.append("Player " + str(i+1))
-player_names = Room_detail(player_name = pn)
+def get_room_code():
+    code = ""
+    possible = "abcdefghijklmnopqrstuvwxyz";
 
-# player_names.save()
+    for char in range(6):
+        code = code + possible[randint(0,len(possible)-1)]
 
+    print(code)
+
+    return code
 
 room_details = Room_detail.objects()
-player_details = Player_detail.objects()
 roles = Role.objects()
 
+app.config["SECRET_KEY"] = "43xf$=DLmQdhWVN*-Yg!s^NM-N&P8WedV"
+
+@app.route("/room/<room_code>")
+def room(room_code):
+    #get room data from mlab in HTML
+    session["room_code"] = room_code
+
+    if "player_id" not in session:
+        session["player_id"] = uuid.uuid4().hex
+
+        #Roll roles:
+        i = randint (0,16)
+        r = roles[i] #Get role
+        session["player_role"] = r["role_name"]
+        session["player_phe"] = r["role_phe"]
+        session["player_ability"] = r["role_ability"]
+
+
+    pd = {
+        "role_name" : session["player_role"],
+        "role_phe" : session["player_phe"],
+        "role_ability" : session["player_ability"]
+    }
+
+    return render_template('room_test.html',Room_detail = Room_detail.objects(acess_code= room_code).first(), pds = pd )
 
 @app.route('/')
 def index():
-    return render_template('index.html')
 
-@app.route('/1234')
-def room():
-    for player in room_details[0]["player_name"]:
-        i = randint(0,16)
-        r = roles[i]
-        p = Player_detail(name = player,
-                        role = r["role_name"],
-                        phe = r["role_phe"],
-                        ability = r["role_ability"])
+    if "room_code" not in session:
+        if "Create" in request.args:
+            #Add_player to room:
+            nop = request.args["nop"]
+            room_code = get_room_code()
+            pn = []
+            for i in range(int(nop)):
+                pn.append("Player " + str(i+1))
+            player_names = Room_detail(player_name = pn,
+                                            acess_code = room_code)
+            player_names.save()
 
-    player_details = Player_detail.objects()
-    return render_template('test.html',Room_detail = Room_detail.objects(), _ = player_details [1] )
+            session["room_code"] = room_code
+
+            return redirect("/room/" + room_code)
+        elif "join" in request.args:
+            room_code = request.args["room_code"]
+            session["room_code"] = room_code
+
+            return redirect("/room/" + room_code)
+
+        return render_template("index.html")
+    else:
+        room_code = session['room_code']
+        return redirect("/room/" + room_code)
 
 if __name__ == '__main__':
   app.run(debug=True)
